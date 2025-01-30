49785d5f92882107d148c44a207afc8a7274a34a
Denis <denisvoitseschuk@gmail.com>
2025-01-30 14:57:07 +0100

    DAY 2 | Styles, msg, login updated

--- js/app/router.js
+++ js/app/router.js
@@ -1,4 +1,5 @@
 import { login } from './pages/login.js';
+import { campaigns } from './pages/campaigns.js';
 // import { campaigns } from './pages/campaigns.js';
 // import { campaign } from './pages/campaign.js';
 // import { users } from './pages/users.js';
@@ -13,7 +14,7 @@ export const router = VueRouter.createRouter({
   routes: [
     { path: '/:pathMatch(.*)*', redirect: '/' }, 
     { path: '/', name: 'Sign in', component: login },
-    // { path: '/campaigns', name: 'Campaigns', component: campaigns },
+    { path: '/campaigns', name: 'Campaigns', component: campaigns },
     // { path: '/campaign/:id', name: 'Campaign', component: campaign },
     // { path: '/users', name: 'Users', component: users },
     // { path: '/user/:id', name: 'User', component: user },
