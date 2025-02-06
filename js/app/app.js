fc014a89b73a44f768a0e795171ba120ce7cd9e9
Denis <denisvoitseschuk@gmail.com>
2025-02-06 23:31:21 +0100

    DAY 5 | POPUP, CAMPAIGN, IMG, CHART

--- js/app/app.js
+++ js/app/app.js
@@ -1,115 +1,110 @@
 import { router } from './router.js';
 import { header } from './widgets/header.js';
-// import { search } from './widgets/search.js';
+import { msg } from './widgets/msg.js';
 import { popup } from './widgets/popup.js';
- import { msg } from './widgets/msg.js';
-import { toogle } from './widgets/toogle.js'; 
-// import { img } from './widgets/img.js';
+import { toogle } from './widgets/toogle.js';
+import { img } from './widgets/img.js';
 
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function(){
     const main = {
-        data(){
+        data() {
             return {
                 url:"https://affiliate.yanbasok.com",
-                user:{name:"",phone:"",email:"",date:"",auth:""},
-                formData:{},
-                title:"",
-                date:"",
-                time:"",
+                user: { name: "", phone: "", email: "", date: "", auth: "" },
+                formData: {},
+                title: "",
+                date: "",
+                time: "",
             }
         },
-        watch:{
-            $route:function(){
-                this.init();
+        watch: {
+            $route: function() {
+              this.init();
             }
         },
-        mounted:function(){
+        mounted: function() {
             this.init();
         },
         methods:{
-            init() {
+            init(){
                 var self = this;
-                if (window.localStorage.getItem('user')) 
-                    self.user = JSON.parse(window.localStorage.getItem('user'));
-        
+                if (window.localStorage.getItem('user')) self.user = JSON.parse(window.localStorage.getItem('user'));
+
                 router.isReady().then(() => {
-                    if (window.localStorage.getItem('user')) {
-                        self.user = JSON.parse(window.localStorage.getItem('user'));
+                    if (window.localStorage.getItem("user")) {
+                        self.user = JSON.parse(window.localStorage.getItem("user"));
                         if (self.$route['path'] == '/' && self.user.type == 'admin') {
                             self.page('/campaigns');
-                        } else if (['/campaigns', '/campaign', '/users', '/user'].includes(self.$route['path']) && self.user.type != 'admin') {
+                        } else if (['/campaigns', '/campaign', '/users', '/user'].includes(self.$route['path'] ) && self.user.type != 'admin') {
                             self.page('/statistics');
-                        } else if (['/statistics', '/payments', '/sites'].includes(self.$route['path']) && self.user.type == 'admin') {
+                        } else if (['/statistics', '/payments', '/sites'].includes(self.$route['path'] ) && self.user.user.type == 'admin') {
                             self.page('/campaigns');
-                        } else if (['/campaigns', '/campaign', '/users', '/user', '/statistics', '/payments', '/sites'].includes(self.$route['path'])) {
+                        } else if (['/campaigns', '/campaign', '/users', '/user', '/statistics', '/payments', '/sites'].includes(self.$route['path'] )) {
                             self.page();
-                        } else {
-                            self.page('/');
+                        } else if (!['/campaigns', '/campaign', '/users', '/user', '/statistics', '/payments', '/sites'].includes(self.$route['path'] )) {
+                            self.page();
                         }
                     } else {
-                        self.page('/');
+                    self.page('/');
                     }
                 });
             },
-            logout() {
-                this.user = { name: "", phone: "", email: "", date: "", auth: "" };
+            logout(){
+                this.user = {name:"", phone:"", email:"", date:"", auth:""};
                 this.page('/');
-                window.localStorage.setItem('user', '');
+                window.localStorage.setItem('user','');
             },
-            scrollTop() {
-                setTimeout(function() {
-                  window.scroll({
-                    top: 0,
-                    behavior: 'smooth'
-                  });
-                }, 50);
-              },
-              
-              scrollBottom() {
-                setTimeout(function() {
-                  window.scroll({
-                    top: 1000,
-                    behavior: 'smooth'
-                  });
-                }, 50);
-              },
-              page: function (path) {
+            scrollTop(){
+                setTimeout(function(){
+                    window.scroll({
+                        top: 0,
+                        behavior: 'smooth'
+                    });
+                },50);
+            },
+            scrollBottom(){
+                setTimeout(function(){
+                    window.scroll({
+                        top: 1000,
+                        behavior: 'smooth'
+                    });
+                },50);
+            },
+            page:function(path=""){
                 this.$router.replace(path);
-                this.title = this.$route.name;
-                document.title = this.$route.name;
-              },
-              toFormData: function(obj) {
+                this.title=this.$route['name'];
+                document.title=this.$route['name'];
+            },
+            toFormData:function(obj){
                 var fd = new FormData();
-              
-                for (var x in obj) {
-                  if (typeof obj[x] === 'object' && obj[x] !== 'img' && obj[x] !== 'copy') {
-                    for (var y in obj[x]) {
-                      if (typeof obj[x][y] === 'object') {
-                        for (var z in obj[x][y]) {
-                          fd.append(x + '[' + y + ']' + '[' + z + ']', obj[x][y][z]);
+
+                for(var x in obj) {
+                    if(typeof obj[x] === 'object' && x !== 'img' && x !== 'copy') {
+                        for(var y in obj[x]) {
+                            if(typeof obj[x] === 'object') {
+                                for(var z in obj[x][y]) {
+                                    fd.append(x + '[' + y + '][' + z + ']', obj[x][y][z]);
+                                }
+                            } else {
+                                fd.append(x + '[' + y + ']', obj[x][y]);
+                            }
                         }
-                      } else {
-                        fd.append(x + '[' + y + ']', obj[x][y]);
-                      }
+                    } else if(x != 'copy') {
+                        fd.append(x, obj[x]);
                     }
-                  } else if (x !== 'copy') {
-                    fd.append(x, obj[x]);
-                  }
                 }
+            
                 return fd;
-              }
+            }
         }
     };
-      
 
-    
     var app = Vue.createApp(main)
-    .component('Header', header)
-    .component('popup', popup)
-    // .component('search', search)
-    .component('msg', msg)
-    .component('toogle', toogle)
-    // .component('Image', img)
+    .component('Header',header)
+    .component('popup',popup)
+    .component('msg',msg)
+    .component('toogle',toogle)
+    .component('Image',img)
     .use(router)
-    .mount('#content');
-})
+    .mount('#content')
+});
--- js/app/widgets/header.js
+++ js/app/widgets/header.js
@@ -41,7 +41,7 @@ export const header = {
               <ul :class="{active:menu==1}" v-if="parent.user && parent.user.type && parent.user.type=='admin'">
                   <li v-if="menu==1" class="al"><i class="fas fa-times" @click="menu=0"></i></li>
                   <li><router-link :class="{'router-link-active':$route.path.search('campaign')==1}" to="/campaigns"><i class="fas fa-bullhorn"></i> Campaigns</router-link></li>
-                  <li><router-link :class="{'router-link-active':$route.path.search('user')==1}" to="/users"><i class="fas fa-user"></i> Users</router-link></li>
+                  <li><router-link :class="{'router-link-active':$route.path.search('user')==1}" to="/users"><i class="fas fa-users"></i> Users</router-link></li>
               </ul>
 
               <!--<ul :class="{active:menu==1}" v-if="parent.user && parent.user.type && parent.user.type!='admin'">
@@ -55,7 +55,9 @@ export const header = {
       </div>
 
           <div class="w30 logo">
+          <router-link :to="'/campaigns/'">
               <img :src="parent.url+'/app/views/images/logo.svg'" />
+          </router-link>
           </div>
       </div>
   </div>
